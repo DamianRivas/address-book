89af02b19dc4ba587ebfd560daff44a32f5107a3
DamianRivas <drivas1993@gmail.com>
2018-01-30 05:16:41 +0000

    Add user ability to delete entries

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,30 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
   end
+
+  describe "#remove_entry" do
+    it "removes only one entry from the address book" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Damian Rivas', '123.456.7890', 'damian@damian.com')
+      book.add_entry('Ioannis Hatzistavros', '333.444.5555', 'ioannis@gmail.com')
+      book.add_entry('Alonso Baltazar', '343.757.9089', 'alonso@live.com')
+      original_size = book.entries.size
+      book.remove_entry('Ioannis Hatzistavros', '333.444.5555', 'ioannis@gmail.com')
+
+      expect(book.entries.size).to eq(original_size - 1)
+    end
+
+    it "doesn't remove an entry if there's no match" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Damian Rivas', '123.456.7890', 'damian@damian.com')
+      book.add_entry('Ioannis Hatzistavros', '333.444.5555', 'ioannis@gmail.com')
+      book.add_entry('Alonso Baltazar', '343.757.9089', 'alonso@live.com')
+      original_size = book.entries.size
+      book.remove_entry('John Doe', '333.444.5555', 'ioannis@gmail.com')
+
+      expect(book.entries.size).to eq(original_size)
+    end
+  end
 end
