02de26d1924404064276a81886fbf901644ae85a
DamianRivas <drivas1993@gmail.com>
2018-02-05 23:10:39 +0000

    Add iterative search to AddressBook

--- models/address_book.rb
+++ models/address_book.rb
@@ -48,4 +48,15 @@ class AddressBook
 
     return nil
   end
+
+  def iterative_search(name)
+
+    entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+    end
+
+    return nil
+  end
 end
